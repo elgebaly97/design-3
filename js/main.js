fc35c11936df791ee599a6b4560919e434f1fa41
elgebaly97 <helgebaly65@gmail.com>
2018-12-08 13:49:18 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,18 @@
+
+
+$(window).on('load', function(){ 
+  
+     $("body").css("overflow","auto");
+    $(".loading").fadeOut(1000);
+    
+
+});
+
+$("body").niceScroll({
+cursorcolor:"aquamarine",
+cursorwidth:"16px"
+});
+
+
+
+
